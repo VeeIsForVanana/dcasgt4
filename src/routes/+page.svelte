6872e528bca9d412360219c53c06d1b105938ec1
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-06 17:10:49 +0800

    fix: change homepage links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    export let data: { posts: any };
+</script>
+
+<h1>Welcome to ThatCrab's Blog!</h1>
+
+{#each data.posts as {name, description}}
+    <a href="/{name}">{name}</a>
+{/each}
