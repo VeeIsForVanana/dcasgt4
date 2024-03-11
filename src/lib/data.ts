97398e44b72a042e631849aa542b34421d9aa865
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-11 11:29:49 +0800

    feat: implement getUser

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,22 +1,37 @@
-type Posts = { name: string, content: string }
+import { json } from "@sveltejs/kit";
 
-const POSTS: Posts[] = [
-    {
-        name: 'post1',
-        content: 'This is the content of Post 1',
-    },
-    {
-        name: 'post2',
-        content: 'This is the content of Post 1',
-    },
-    {
-        name: 'post3',
-        content: 'This is the content of Post 1',
-    }
-]
+type User = { 
+    username: string,
+    description: string,
+    userURL: string,
+    avatarURL: string,
+}
 
-export function getPosts() {
-    return [...POSTS];
+export async function getUsers(): Promise<User[]> {
+    
+}
+
+export async function getUser(username: string): Promise<User> {
+    return fetch(`https://api.github.com/users/${username}`).then(
+        response => {
+            if (!response.ok) {
+                throw new Error (`Network errored with status code: ${response.status} ${response.statusText}`)
+            }
+            return response.json()
+        },
+        error => {
+            console.error(`Error occurred while fetching user ${username}, ${error}`)
+        }
+    ).then(
+        json => {
+            return {
+                username: json["login"],
+                description: json["bio"],
+                userURL: json["user_url"],
+                avatarURL: json["avatar_url"],
+            }
+        }
+    )
 }
 
 export function addNewPost(name: string, content: string): void {
