795e6349f9427846355a1c146977950146119131
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-15 03:00:10 +0800

    feat: add error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { page } from '$app/stores'
+</script>
+
+<h1>{$page.status} {$page.error?.message}</h1>
+
+<img alt={`cat but ${$page.status}`} src={`https://http.cat/${$page.status}`} />
