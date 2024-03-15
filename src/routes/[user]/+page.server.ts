ff8da045bca1e3ba0543a0c074eb59e83a9b9821
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-15 20:31:05 +0800

    fix: thank u basti

--- src/routes/[user]/+page.server.ts
+++ src/routes/[user]/+page.server.ts
@@ -1,15 +1,15 @@
 import { jsonToUser } from '$lib/data.js'
 import { error } from '@sveltejs/kit'
 
-export async function load({ fetch, params }) {
-    return fetch(`https://api.github.com/users/${params.user}`).then(
-        response => {
-            if (!response.ok) {
-                throw error (response.status, `${response.statusText} on user ${params.user}`)
-            }
-            return response.json()
-        }
-    ).then(
-        jsonToUser
-    )
+export async function load({ fetch, params: { user }, cookies }) {
+    
+    const history = cookies.get('last_visited');
+    const users = typeof history === 'undefined' ? [user] : history.split(',').concat([user]);
+    const stored = users.toString(); 
+    cookies.set('last_visited', stored, { path: '/' });
+    
+    const response = await fetch(`https://api.github.com/users/${user}`);
+    if (!response.ok) error(response.status, `${response.statusText} on user ${user}`);
+    const json = await response.json();
+    return { user: jsonToUser(json), history: stored };
 }
