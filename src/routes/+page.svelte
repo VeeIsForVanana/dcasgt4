67c8868e1e069164fa1d76fe4dceae5beeaec4e9
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-15 13:53:02 +0800

    feat: implement rate limit check

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@
 
 <h1>GitHub Users</h1>
 
+<h3>{data.rateLimits.per_hour} requests per hour allowed, you have {data.rateLimits.remaining} left and it will reset at {data.rateLimits.reset_time}</h3>
+
 {#each data.users as {username, id, userURL, avatarURL}}
     <a href="/{username}">{id} - {username}</a><br>
 {/each}
