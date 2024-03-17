fe18d38872c0b8b9f8cf6b20ae5ff1499ac97a14
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-17 13:25:07 +0800

    feat: add link tests

--- tests/test.ts
+++ tests/test.ts
@@ -5,4 +5,27 @@ test.describe('initial tests', async () => {
 		await page.goto('/');
 		await expect(page.getByRole('heading', { name: 'GitHub Users' })).toBeVisible();
 	});
+
+	// test first and last links to keep rate limit sane
+	test('index page has first link that corresponds to a github user', async ({ page }) => {
+		await page.goto('/');
+		const linkText = await page.getByRole('link').first().textContent()
+		const username = linkText?.split(' - ')[1]
+		const response = await fetch(`https://api.github.com/users/${username}`)
+		expect(response.ok)
+	})
+
+	// can't test all links otherwise we will hit the rate limit too quickly
+	test('index page has at least 10 links', async ({ page }) => {
+		await page.goto('/');
+		expect((await page.getByRole('link').all()).length >= 10)
+	})
+
+	test('index page has last link taht corresponds to a github user', async({ page }) => {
+		await page.goto('/');
+		const linkText = await page.getByRole('link').last().textContent()
+		const username = linkText?.split(' - ')[1]
+		const response = await fetch(`https://api.github.com/users/${username}`)
+		expect(response.ok)
+	})
 })
