791b48b389964347a8bb7a12f936c24e0a7fd592
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-03-17 14:49:13 +0800

    fix: fix homepage tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test.describe('initial tests', async () => {
+test.describe('homepage tests', async () => {
 	test('index page has expected h1', async ({ page }) => {
 		await page.goto('/');
 		await expect(page.getByRole('heading', { name: 'GitHub Users' })).toBeVisible();
@@ -13,19 +13,23 @@ test.describe('initial tests', async () => {
 		const username = linkText?.split(' - ')[1]
 		const response = await fetch(`https://api.github.com/users/${username}`)
 		expect(response.ok)
-	})
+	});
 
 	// can't test all links otherwise we will hit the rate limit too quickly
 	test('index page has at least 10 links', async ({ page }) => {
 		await page.goto('/');
 		expect((await page.getByRole('link').all()).length >= 10)
-	})
+	});
 
-	test('index page has last link taht corresponds to a github user', async({ page }) => {
+	test('index page has last link that corresponds to a github user', async({ page }) => {
 		await page.goto('/');
 		const linkText = await page.getByRole('link').last().textContent()
 		const username = linkText?.split(' - ')[1]
 		const response = await fetch(`https://api.github.com/users/${username}`)
 		expect(response.ok)
-	})
+	});
+})
+
+test.describe('userpage content tests', async () => {
+	
 })
